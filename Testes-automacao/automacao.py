b0ae779c8a2ef80920d56dc3a6818af8dbaf1add
Dados-Controladoria <mateus.facioli@magazineluiza.com.br>
2025-03-20 16:31:33 -0300

    Desenvolvimento dos logs de execução do script de automação

--- Testes-automacao/automacao.py
+++ Testes-automacao/automacao.py
@@ -1,13 +1,24 @@
-
 import subprocess
 import logging
 
+# Configuração do log
+logging.basicConfig(
+    filename="processo.log", 
+    level=logging.INFO, 
+    encoding="utf-8", 
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S")
 
-logging.basicConfig(filename="processo.log", level=logging.INFO)
+logging.info("O script está sendo executado...")
 
-resultado = subprocess.run(["python", "main.py"], capture_output=True, text=True)
-logging.info(resultado.stdout)
+# Executa o script e captura a execução do arquivo e se deu erro ou não. Caso der erro, vai retornar o erro que deu.
+resultado = subprocess.run(["python", "main.py"], capture_output=True, text=True, universal_newlines=True)
 
+# Captura erros (stderr)
+if resultado.stderr.strip():
+    logging.error(f"Erro do script:\n{resultado.stderr}")
+else:
+    logging.info("O script 'main.py' foi executado com sucesso, sem erros.")
 
-
+    # logging.info(resultado.stdout) - usar esse módulo do logging pra retornar a saída do arquivo assim que a formatação dos dados for realizada.
 
--- Testes-automacao/main.py
+++ Testes-automacao/main.py
@@ -4,14 +4,16 @@ import os
 import logging
 import subprocess
 import logging
+import sys
 
 
 
 def cronometro ():
     segundos = 0
     minutos = 0
-    for i in range(120):
-        print(f"{minutos}:{segundos}")  
+    for i in range(60):
+        sys.stdout.write(f"{minutos:00}:{segundos:00}\n")
+        sys.stdout.flush()  
         time.sleep(1)
         segundos+= 1
         if  segundos == 60:
