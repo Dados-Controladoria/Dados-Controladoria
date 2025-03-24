a4263337f6acc589d19f224f3f18cf088f9616ad
Dados-Controladoria <mateus.facioli@magazineluiza.com.br>
2025-03-24 17:22:28 -0300

    Criação de tratamento de erros por interrupção humana ou inesperada

--- Testes-automacao/automacao.py
+++ Testes-automacao/automacao.py
@@ -1,5 +1,6 @@
 import subprocess
 import logging
+import sys
 
 # Configuração do log
 logging.basicConfig(
@@ -11,14 +12,28 @@ logging.basicConfig(
 
 logging.info("O script está sendo executado...")
 
-# Executa o script e captura a execução do arquivo e se deu erro ou não. Caso der erro, vai retornar o erro que deu.
-resultado = subprocess.run(["python", "main.py"], capture_output=True, text=True, universal_newlines=True)
+try:
+    
+    # Executa o script e captura a execução do arquivo e se deu erro ou não. Caso der erro, vai retornar o erro que deu.
+    resultado = subprocess.run(["python", "main.py"], capture_output=True, text=True, universal_newlines=True)
 
-# Captura erros (stderr)
-if resultado.stderr.strip():
-    logging.error(f"Erro do script:\n{resultado.stderr}")
-else:
-    logging.info("O script 'main.py' foi executado com sucesso, sem erros.")
+    # Captura erros (stderr)
+    if resultado.stderr.strip():
+        logging.error(f"Erro do script:\n{resultado.stderr}")
+    else: 
+        logging.info("O script 'main.py' foi executado com sucesso, sem erros.")
+except KeyboardInterrupt:
+    logging.warning("Execução interrompida pelo usuário.")
+    sys.exit(1)
+except Exception as e:
+    logging.error(f"Execução encerrada devido a erro inesperado: {e}")
+    sys.exit(1)
 
-    # logging.info(resultado.stdout) - usar esse módulo do logging pra retornar a saída do arquivo assim que a formatação dos dados for realizada.
+finally:
+    logging.info("Execução encerrada.")
+
+
+
+
+ # logging.info(resultado.stdout) - usar esse módulo do logging pra retornar a saída do arquivo assim que a formatação dos dados for realizada.
 
