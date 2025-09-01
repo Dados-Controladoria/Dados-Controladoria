fb4ad5f4f0f6d9aa50f19b5f0adf361016bbb830
Dados-Controladoria <mateus.facioli@magazineluiza.com.br>
2025-09-01 17:55:29 -0300

    Ajuste dos meses query magalu

--- atualiza_balancete_project/src/processador_ETL_magalu.py
+++ atualiza_balancete_project/src/processador_ETL_magalu.py
@@ -62,7 +62,7 @@ try:
      and ffvv.flex_value_set_id         IN (1014008,1016808)
      and gb.ledger_id                   IN (2022,2042)
      and gb.actual_flag                 IN ('A')
-     and gb.period_name                 IN ('06-25', '07-25')
+     and gb.period_name                 IN ('08-25', '09-25')
      and gcc.segment3                   NOT LIKE '%ORC%'
      and gcc.segment1                   IN ('001','004','007','009','010','011', '012')
      
